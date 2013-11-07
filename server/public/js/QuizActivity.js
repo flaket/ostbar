4c9c5e23b5c46391d498e8bdf9befaca7a16dc3b
Nitharshaan Thevarajah <nitharshaan@gmail.com>
2013-11-07 15:09:16 +0100

    quiz activity

--- server/public/js/QuizActivity.js
+++ server/public/js/QuizActivity.js
@@ -1,5 +1,14 @@
 function QuizActivity(){
+	this.questionsMade = 0;
+	this.list = [];
+}
 
+function Question(name, alt1, alt2, alt3, checkboxChecked){
+	this.questionName = name;
+	this.questionAlternative1 = alt1;
+	this.questionAlternative2 = alt2;
+	this.questionAlternative3 = alt3;
+	this.questionCorrectAnswer = checkboxChecked;
 }
 
 function createNewQuizActivity(quizObject){
@@ -7,5 +16,53 @@ function createNewQuizActivity(quizObject){
 }
 
 function makeNewQuestion(){
-	$(".quizQuestions").clone().insertAfter("div.quizQuestions:last");
+	createAnotherQuestion(currentQuestionObject);
+}
+
+
+var currentQuestionObject = new QuizActivity();
+
+function createAnotherQuestion(questionObject){
+	currentQuestionObject = questionObject;
+
+	resetFields();
+
+}
+
+function resetFields(){
+	if( (!$("#quizQuestion").val().length > 0) || (!$("#alt1").val().length > 0) || (!$("#alt2").val().length > 0) || (!$("#alt3").val().length > 0) 
+		|| ( (!$("#checkAlt1").is(":checked")) && (!$("#checkAlt2").is(":checked")) && (!$("#checkAlt3").is(":checked")) ) ){
+		alert("fyll inn verdier i alle felt"); 
+	}
+	else{
+		currentQuestionObject.questionsMade++;
+
+		var questionName = $("#quizQuestion").val();
+		var alt1 = $("#alt1").val();
+		var alt2 = $("#alt2").val();
+		var alt3 = $("#alt3").val();
+		var checkedAlternative = null;
+
+		if($("#checkAlt1").is(":checked")){
+			checkedAlternative = "alternative 1";
+		}
+		else if($("#checkAlt2").is(":checked")){
+			checkedAlternative = "alternative 2";
+		}
+		else{ checkedAlternative = "alternative 3"; }
+
+		$("input:text").val("");
+		$("input:checkbox").attr("checked", false);
+
+		var tempObject = new Question(questionName, alt1, alt2, alt3, checkedAlternative);
+		currentQuestionObject.list.push(tempObject);
+
+		console.log("number of questions made : " + currentQuestionObject.questionsMade);
+		console.log(tempObject);
+		console.log(currentQuestionObject.list);
+	}
+}
+
+function finalizeQuiz(){
+	
 }
