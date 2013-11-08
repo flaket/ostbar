ea381e2790f966b1dcf222f3ba35541303484744
Eivind H <evdh15@gmail.com>
2013-11-08 13:04:31 +0100

    updated missing objectlist fields

--- server/public/js/ElementFunctions.js
+++ server/public/js/ElementFunctions.js
@@ -1,5 +1,5 @@
 function dialogFunction(e){
-	$("#writtenText").val(objectList[inList(objectList,e.target)].dialogData);
+	$("#writtenText").val(currentObjectList.objectList[inList(currentObjectList.objectList,e.target)].dialogData);
 	$(".writtenDialog").dialog({
 		modal: true,
 		buttons: {
@@ -11,11 +11,11 @@ function dialogFunction(e){
 }
 
 function animationFunction(e){
-	runAnimationEffect(objectList[inList(objectList,e.target)]);
+	runAnimationEffect(currentObjectList.objectList[inList(currentObjectList.objectList,e.target)]);
 }
 
 function mathActivityFunction(e){
-	createMathActivity(objectList[inList(objectList,e.target)].activityObject);
+	createMathActivity(currentObjectList.objectList[inList(currentObjectList.objectList,e.target)].activityObject);
 }
 
 function languageActivityFunction(){
