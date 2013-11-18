46310bcd95814850a81f05a609216b2ce321be37
Nitharshaan Thevarajah <nitharshaan@gmail.com>
2013-11-18 09:04:25 +0100

    started on language activity

--- server/public/js/LanguageActivity.js
+++ server/public/js/LanguageActivity.js
@@ -2,12 +2,34 @@ function LanguageActivity(){
 	
 }
 
-function createNewLanguageActivity(languageObject){
+function createNewLanguageActivity(languageObject, isNewGame){
 	
 }
 
-function createLanguageActivity(){
-	$(".chooseActivityDialog").dialog("close");
+function createLanguageActivity(languageObject){
 
-	console.log("language activity");
+}
+
+function initializeLanguageDialog(){
+	var Wwidth = $(window).width();
+	var Wheight = $(window).height();
+	
+	$(".languageActivity").dialog({
+		rezisable: false,
+		height: Wheight*0.8,
+		width: Wwidth*0.7,
+		position: {
+			my: "center top",
+			at: "center top",
+			of: "#mainFrame"
+		},
+		show: {
+			effect: "clip",
+			duration: 500
+		},
+		hide: {
+			effect: "clip",
+			duration: 500
+		}
+	});
 }
