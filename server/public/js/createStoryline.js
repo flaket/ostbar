2fb4b77d25ca24ed6b6256ca8e09d3be66ad40ed
Peter Ringset <peterringset@gmail.com>
2013-11-18 14:49:33 +0100

    .

--- server/public/js/createStoryline.js
+++ server/public/js/createStoryline.js
@@ -1,5 +1,4 @@
 $(function createStorylineView(){
-	
 	$("#storylineDialog").dialog({
 		autoOpen: false,
 		width: 900,
@@ -15,6 +14,28 @@ $(function createStorylineView(){
 	});
 	
 	$("#storylineButton").on("click", function(){
+		var html = '';
+		for (key in sceneList){
+			var scene = sceneList[ key ];
+			var scenetype = scene.sceneType;
+			var sceneId = scene.sceneId;
+			var div = '<div class="img-wrapper img-wrapper1" name="'+sceneId+'"><div class="img-container">';
+			div += '<img name="' + scenetype.sceneTypeId + '" src="' + scenetype.backgroundAvatar.url + '" width ="200" height="200">';
+			div += '</div></div>';
+
+			html += div;
+
+			console.log('scene', scene);
+			for ( key in scene.elements ){
+				var element = scene.elements[ key ];
+				console.log('element', element);
+				if ( element.actionTypes.length ){
+
+				}
+			}
+		}
+		$('#storylineDialog').html(html);
+
 		$("#storylineDialog").dialog("open");
 	});
 });
