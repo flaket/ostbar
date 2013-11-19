128db87da3d9368f0d8d8bf11609e3eb0ea87c12
Peter Ringset <peterringset@gmail.com>
2013-11-19 13:10:58 +0100

    createStoryline

--- server/public/js/createStoryline.js
+++ server/public/js/createStoryline.js
@@ -10,46 +10,76 @@ $(function createStorylineView(){
 		hide: {
 	      effect: "clip",
 	      duration: 300
+    	},
+    	buttons: {
+    		'Lukk': function (){
+    			$( this ).dialog( 'close' ); 
+    		}
     	}
 	});
 	
 	$("#storylineButton").on("click", function(){
-		var html = '';
-		for ( sceneNr in sceneList ){
-			var scene = sceneList[ sceneNr ];
-			var scenetype = scene.sceneType;
-			var sceneId = scene.sceneId;
-			var div = '<div class="img-wrapper img-wrapper1" name="'+sceneId+'" style="width: 100%; height: 200px"><div class="img-container" style="width: 25%">';
-			div += '<img name="' + scenetype.sceneTypeId + '" src="' + scenetype.backgroundAvatar.url + '" style="">';
-			div += '</div>';
+		getUpdatedElements( function ( error, success ){
+			if ( error ) return;
 
-			for ( elementNr in scene.elements ){
-				var element = scene.elements[ elementNr ];
+			var html = '';
+		
+			for ( sceneNr in sceneList ){
+				var scene = sceneList[ sceneNr ];
+				var scenetype = scene.sceneType;
+				var sceneId = scene.sceneId;
+				var div = '<div class="img-wrapper img-wrapper1" name="'+sceneId+'" style="width: 100%; height: 200px"><div class="img-container" style="width: 25%">';
+				div += '<img name="' + scenetype.sceneTypeId + '" src="' + scenetype.backgroundAvatar.url + '" style="">';
+				div += '</div>';
 
-				var names = new Array();
-				for ( actionTypeNr in element.actionTypes ){
-					var actionType = element.actionTypes[ actionTypeNr ];
+				var names = {};
 
-					switch ( actionType.name ){
-						case 'TO_ACTIVITY': names.push('Aktivitet'); break;
-						case 'DIALOG': names.push('Dialog'); break;
-						case 'ANIMATION': names.push('Animasjon'); break;
-						case 'SCENE': names.push('Til scene'); break;
+				for ( elementNr in scene.elements ){
+					var element = scene.elements[ elementNr ];
+
+
+					for ( actionTypeNr in element.actionTypes ){
+						var actionType = element.actionTypes[ actionTypeNr ];
+
+						var name;
+
+						switch ( actionType.name ){
+							case 'TO_ACTIVITY': name = 'Aktivitet'; break;
+							case 'DIALOG': name = 'Dialog'; break;
+							case 'ANIMATION': name = 'Animasjon'; break;
+							case 'TO_SCENE': name = 'Til scene'; break;
+						}
+
+
+						if ( names.hasOwnProperty( name ) ){
+							names[name]++;
+						} else {
+							names[name] = 1;
+						}
 					}
 				}
 
-				div += '<div style="width: 75%; float: right;">' + names.toString() + '</div>';
+				var namesString = "";
 
+				for ( prop in names ){
+					var count = names[prop];
 
-				console.log('element', element);
+					console.log('count is', count, 'for', prop);
+
+					namesString += prop;
+					if ( count > 1 ) namesString += ' (' + count + ')';
+					namesString += '<br>';
+				}
+
+				div += '<div style="width: 75%; float: right;">' + namesString + '</div>';
+
+				div += '</div>';
+
+				html += div;
 			}
+			$('#storylineDialog').html(html);
 
-			div += '</div>';
-
-			html += div;
-		}
-		$('#storylineDialog').html(html);
-
-		$("#storylineDialog").dialog("open");
+			$("#storylineDialog").dialog("open");
+		});
 	});
 });
